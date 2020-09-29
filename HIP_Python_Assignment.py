0c26f7e0c5a27c8067fa379f50a9025568d05650
wahidawari <wahidullahd@gmail.com>
2020-09-28 19:44:50 -0700

    edit HIP_Python_Assignment.py

--- HIP_Python_Assignment.py
+++ HIP_Python_Assignment.py
@@ -4,12 +4,16 @@ Local_tax = 1.095
 print("Welcome to Self Checkout!")
 print("Please Enter Item Prices Individually.")
 print("When Done, Enter: 0")
+
+
 #storing the prices
 #Loop until customer enters 0 
 price = float(input("Enter First Item price: $"))
 while price != 0: 
   total = float(price + total)
   price = float( input("Enter Next Item Price: $"))
+
+
 #Adding on Tax
 tax = input("Is Sales Tax Applicable? (y/n): ").lower()
 print("")
@@ -18,6 +22,8 @@ if tax == "y" :
   print("")
   Cash = float(input("Enter in Money Provided by Customer: $"))
   print("")
+
+
   #if they pay less than the total:
   while Cash < round(total * Local_tax, 2): 
    print("You still need to pay: $", round(((total * Local_tax) - Cash),2), sep = "")
@@ -25,17 +31,23 @@ if tax == "y" :
    print("Your Total is: $", format(total * Local_tax, ",.2f"), sep = "")
    print("")
    Cash = float(input("Enter in Money Provided by Customer: $"))
+
+
   #If they pay more or equal
   final_total = round(total * Local_tax, 2)
   print("Your Change is: $", format(Cash - ((final_total)), ",.2f"), sep = "")
   print("")
   print("Thank You for Shopping Here!")
+
+
 #What if there is no tax
 else: 
   print("Your total is: $", format(total, ",.2f"), sep = "")
   print("")
   Cash = float(input("Enter in Money Provided by Customer: $"))
   print("")
+
+
   #if they pay less than the total:
   while Cash < total : 
    print("You still need to pay: $", format(((total) - Cash), ",.2f"), sep = "")
@@ -43,6 +55,8 @@ else:
    print("Your Total is: $", total, sep = "")
    print("")
    Cash = float(input("Enter in Money Provided by Customer: $"))
+
+   
   #what if they pay more or the exact amount: 
   if Cash >= total:
     print("Your Change is: $",  format(Cash - (total ),",.2f"), sep = "")
